78e40cb2ed852af8163b7334cb2adeeace9cc0e9
YeheyaBoy <airadzakyikhwan@gmail.com>
2025-12-15 21:59:01 +0700

    move, cleanup file, and adding folders

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+ using System.Collections;
+using System.Collections.Generic;   
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private Vector2 _movement;
+
+    private Rigidbody2D _rb;
+    private Animator _animator;
+
+    private const string _horizontal = "horizontal";
+    private const string _vertical = "vertical";
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
+
+        _rb.velocity = _movement * _moveSpeed;
+
+        _animator.SetFloat(_horizontal, _movement.x);
+        _animator.SetFloat(_vertical, _movement.y);
+    }
+}
